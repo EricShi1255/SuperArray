22261774edf1025ced39938fedfa4bb0c8b652e7
EricShi1255 <eshi20@stuy.edu>
2020-11-01 12:37:36 -0500

    add()

--- SuperTester.java
+++ SuperTester.java
@@ -1,18 +1,9 @@
 public class SuperTester {
-    public static int size(String[] data) {
-        int count = 0;
-        for (int i = 0; i < data.length; i++) {
-            if (data[i] != null) {
-                count ++;
-            }
-        }
-        return count;
-   }
     public static void main(String[] args) {
-        String[] poo = new String[10];
-        poo[0] = "a";
-        poo[1]= "b";
-        System.out.println(size(poo));
+        SuperArray a = new SuperArray();
+        a.add("a");
+        a.add("b");
+        System.out.println(a.size());
     }
     
 
